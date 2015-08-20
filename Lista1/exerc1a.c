480a1d5bb173b1d5993da3bbbe26a83205bda421
Brunno <Mano21@users.noreply.github.com>
2015-08-20 11:19:16 -0300

    Update exerc1a.c

--- Lista1/exerc1a.c
+++ Lista1/exerc1a.c
@@ -1,30 +1,43 @@
-//cc -o exe1 exe1.c
-#include <stdlib.h>
+
+
+SOP/lista_fork01/fork1A.c
+@AndressaUmetsu AndressaUmetsu 5 days ago 15/08/15
+
+1 contributor
+37 lines (34 sloc) 0.515 kB
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*  A
+|   |   | 
+B   C   D */
+
 int main(){
-	pid_t f1,f2,f3,f4;
-
-	f1 =fork();
-
-	if(f1==0){
-		printf("A\n");
-	
+	pid_t f;
+	//Pai A	
+	printf("Pai (PID=%u) \n", getpid());
+	printf("A");
+	f = fork();
+	//Filho B	
+	if (f == 0){ //se retorno de f igual a 0 então é o filho
+		printf("B");
+	    exit(0);
 	}
-	}else{
-
-	if(f2==0){
-		printf("B\n");
+	f = fork();
+	//Filho C	
+	if (f == 0){
+	    printf("C");
+	    exit(0);
 	}
-	if(f3==0){
-		printf("C\n");
+	f = fork();
+	//Filho D	
+	if (f == 0){
+	    printf("D");
+	    exit(0);
 	}
-	if(f4==0){
-		printf("D\n");
-
-	}
-return 0;
+	//sleep(10);
+    return 0;
 }
